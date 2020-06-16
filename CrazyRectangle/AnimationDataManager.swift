976d4cabecfb146e8a840c662b4a522a3ed24a72
Pavel Parshin <mail@pavelparshin.ru>
2020-06-16 14:55:27 +0300

    create DataManager for Spring animation

---
+++ CrazyRectangle/AnimationDataManager.swift
@@ -0,0 +1,29 @@
+//
+//  AnimationDataManager.swift
+//  CrazyRectangle
+//
+//  Created by Pavel Parshin on 16.06.2020.
+//  Copyright © 2020 Pavel Parshin. All rights reserved.
+//
+
+class AnimationDataManager {
+
+    static let shared = AnimationDataManager()
+    
+    private init() {}
+    
+    let animations = ["slideLeft", "slideRight", "slideDown", "slideUp",
+                      "squeezeLeft", "squeezeRight", "squeezeDown", "squeezeUp",
+                      "fadeIn", "fadeOut", "fadeOutIn", "fadeInLeft",
+                      "fadeInRight", "fadeInDown", "fadeInUp", "zoomIn",
+                      "zoomOut", "fall", "shake", "pop", "flipX", "flipY",
+                      "morph", "squeeze", "flash", "wobble", "swing"]
+    
+    let curves = ["easeIn", "easeOut", "easeInOut", "linear", "spring",
+                  "easeInSine", "easeOutSine", "easeInOutSine", "easeInQuad",
+                  "easeOutQuad", "easeInOutQuad", "easeInCubic", "easeOutCubic",
+                  "easeInOutCubic", "easeInQuart", "easeOutQuart", "easeInOutQuart",
+                  "easeInQuint", "easeOutQuint", "easeInOutQuint", "easeInExpo",
+                  "easeOutExpo", "easeInOutExpo", "easeInCirc", "easeOutCirc",
+                  "easeInOutCirc", "easeInBack", "easeOutBack", "easeInOutBack"]
+}
---
+++ CrazyRectangle/Model/AnimationModel.swift
@@ -0,0 +1,19 @@
+//
+//  AnimationModel.swift
+//  CrazyRectangle
+//
+//  Created by Pavel Parshin on 16.06.2020.
+//  Copyright © 2020 Pavel Parshin. All rights reserved.
+//
+
+struct AnimationModel {
+    var animation: String
+    var curve: String
+    var force: Float
+    var delay: Float
+    var duration: Float
+    var velocity: Float
+    var repeatCount: Float
+    var rotate: Float
+    var opacity: Float
+}
